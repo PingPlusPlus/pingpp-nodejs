64ad127ea9d29a00e76c1ff460a33b7cb223ada5
afon <wengxufeng@gmail.com>
2016-12-02 02:10:11 +0800

    移除 event list 接口

--- example/event.js
+++ example/event.js
@@ -1,5 +1,5 @@
 // api_key 获取方式：登录 [Dashboard](https://dashboard.pingxx.com)->点击管理平台右上角公司名称->开发信息-> Secret Key
-var API_KEY = "sk_test_ibbTe5jLGCi5rzfH4OqPW9KC"
+var API_KEY = 'sk_test_ibbTe5jLGCi5rzfH4OqPW9KC';
 // 设置 api_key
 var pingpp = require('../lib/pingpp')(API_KEY);
 
@@ -8,18 +8,7 @@ pingpp.events.retrieve(
   'evt_VzWdLFbm5D6OdOuzQv7oqF0X',
   function(err, event) {
     if (err != null) {
-      console.log("pingpp.events.retrieve fail:", err);
-    }
-    // YOUR CODE
-  }
-);
-
-// list all events
-pingpp.events.list(
-  { limit: 3 },
-  function(err, events) {
-    if (err != null) {
-      console.log("pingpp.events.list fail:", err);
+      console.log('pingpp.events.retrieve fail:', err);
     }
     // YOUR CODE
   }
