caf4690a9ef85458b1f112d34e3f7ca0950df2b9
afon <wengxufeng@gmail.com>
2017-04-21 15:08:46 +0800

    update example

--- example/pay.js
+++ example/pay.js
@@ -5,9 +5,9 @@
 // 该代码仅供学习和研究 Ping++ SDK 使用，仅供参考。
 
 // api_key 获取方式：登录 [Dashboard](https://dashboard.pingxx.com)->点击管理平台右上角公司名称->开发信息-> Secret Key
-var API_KEY = "sk_test_ibbTe5jLGCi5rzfH4OqPW9KC"
+var API_KEY = 'sk_test_ibbTe5jLGCi5rzfH4OqPW9KC';
 // app_id 获取方式：登录 [Dashboard](https://dashboard.pingxx.com)->点击你创建的应用->应用首页->应用 ID(App ID)
-var APP_ID = "app_1Gqj58ynP0mHeX1q"
+var APP_ID = 'app_1Gqj58ynP0mHeX1q';
 // 设置 api_key
 var pingpp = require('../lib/pingpp')(API_KEY);
 // pingpp.parseHeaders(/*headers*/); // 把从客户端传上来的 Headers 传到这里
@@ -29,18 +29,18 @@ pingpp.setPrivateKeyPath(__dirname + '/your_rsa_private_key.pem');
 var channel = 'alipay'; // 支付使用的第三方支付渠道取值，请参考：https://www.pingxx.com/api#api-c-new
 var extra = {};
 switch (channel) {
-  case 'alipay_wap':
-    extra = {
-      // success_url 和 cancel_url 在本地测试不要写 localhost ，请写 127.0.0.1。URL 后面不要加自定义参数
-      'success_url': 'http://www.yourdomain.com/success',
-      'cancel_url': 'http://www.yourdomain.com/cancel'
-    };
-    break;
-  case 'upacp_wap':
-    extra = {
-      'result_url': 'http://www.yourdomain.com/result'// 银联同步回调地址
-    };
-    break;
+case 'alipay_wap':
+  extra = {
+    // success_url 和 cancel_url 在本地测试不要写 localhost ，请写 127.0.0.1。URL 后面不要加自定义参数
+    'success_url': 'http://www.yourdomain.com/success',
+    'cancel_url': 'http://www.yourdomain.com/cancel'
+  };
+  break;
+case 'upacp_wap':
+  extra = {
+    'result_url': 'http://www.yourdomain.com/result'// 银联同步回调地址
+  };
+  break;
   // 请根据 API 文档说明，根据不同渠道，添加不同的 extra 参数。
 }
 var crypto = require('crypto');
@@ -53,14 +53,16 @@ pingpp.charges.create({
   app:       { id: APP_ID },
   channel:   channel,// 支付使用的第三方支付渠道取值，请参考：https://www.pingxx.com/api#api-c-new
   amount:    100,//订单总金额, 人民币单位：分（如订单总金额为 1 元，此处请填 100）
-  client_ip: "127.0.0.1",// 发起支付请求客户端的 IP 地址，格式为 IPV4，如: 127.0.0.1
-  currency:  "cny",
-  subject:   "Your Subject",
-  body:      "Your Body",
+  client_ip: '127.0.0.1',// 发起支付请求客户端的 IP 地址，格式为 IPV4，如: 127.0.0.1
+  currency:  'cny',
+  subject:   'Your Subject',
+  body:      'Your Body',
   extra:     extra
 }, function(err, charge) {
   if (err != null) {
-    console.log("pingpp.charges.create fail:", err);
+    console.log('pingpp.charges.create fail:', err);
+  } else {
+    console.log(charge);
   }
   // YOUR CODE
 });
